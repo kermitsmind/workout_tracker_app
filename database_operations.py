52cf294e164138599980be36e2d2899358016727
kermitsmind <kermitsmind@icloud.com>
2022-01-18 22:22:03 +0100

    refactorized code + created EER diagram + formatting

---
+++ database_operations.py
@@ -0,0 +1,146 @@
+#### importing libraries
+from __future__ import print_function
+import mysql.connector
+from mysql.connector import errorcode
+
+
+#### open connection with given credentials and return the connection object
+def openConnectionToDB(host, user, password, database):
+    try:
+        connection = mysql.connector.connect(
+            host=host, user=user, password=password, database=database
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    return connection
+
+
+#### close given connection
+def closeConnectionToDB(connection):
+    try:
+        connection.close()
+    except:
+        print("Encountered error while closing the connection")
+
+
+def createDatabase(host, user, password):
+    #### connect to the database
+    try:
+        connection = mysql.connector.connect(host=host, user=user, password=password)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+
+    #### create cursor for further usage
+    mycursor = connection.cursor()
+
+    #### create database
+    mycursor.execute("CREATE DATABASE WorkoutTrackerDB")
+
+
+def createDatabaseTables(host, user, password, database):
+    #### connect to the database
+    connection = openConnectionToDB(
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+    )
+
+    #### create cursor for further usage
+    mycursor = connection.cursor()
+
+    #### select DB
+    mycursor.execute("use WorkoutTrackerDB")
+
+    ### create tables
+    table_1 = """CREATE TABLE personal_information (
+        person_id int auto_increment primary key,
+        first_name varchar(90),
+        last_name varchar(90),
+        birth_date date,
+        phone_number int default null,
+        nationality varchar(90),
+        registration_date date
+    )"""
+    mycursor.execute(table_1)
+
+    table_2 = """CREATE TABLE diet (
+        diet_id int auto_increment primary key,
+        person_id int,
+        name varchar(90),
+        start_date date,
+        stop_date date,
+        calories int default null,
+        foreign key (person_id) references personal_information(person_id)
+    )"""
+    table_2_cst = "ALTER TABLE diet MODIFY person_id int NOT NULL"
+    mycursor.execute(table_2)
+    mycursor.execute(table_2_cst)
+
+    table_3 = """CREATE TABLE running (
+        running_id int auto_increment primary key,
+        person_id int,
+        date date,
+        type varchar(90),
+        total_time int default null,
+        total_distance int default null,
+        terrain varchar(90),
+        foreign key (person_id) references personal_information(person_id)
+    )"""
+    table_3_cst = "ALTER TABLE running MODIFY person_id int NOT NULL"
+    mycursor.execute(table_3)
+    mycursor.execute(table_3_cst)
+
+    table_4 = """CREATE TABLE swimming (
+        swimming_id int auto_increment primary key,
+        person_id int,
+        date date,
+        type varchar(90),
+        total_time int default null,
+        total_distance int default null,
+        water varchar(90),
+        foreign key (person_id) references personal_information(person_id)
+    )"""
+    table_4_cst = "ALTER TABLE swimming MODIFY person_id int NOT NULL"
+    mycursor.execute(table_4)
+    mycursor.execute(table_4_cst)
+
+    table_5 = """CREATE TABLE weight_lifting (
+        weight_lifting_id int auto_increment primary key,
+        person_id int,
+        date date,
+        type varchar(90),
+        no_series int default null,
+        repeats_per_series int default null,
+        weight int default null,
+        foreign key (person_id) references personal_information(person_id)
+    )"""
+    table_5_cst = "ALTER TABLE weight_lifting MODIFY person_id int NOT NULL"
+    mycursor.execute(table_5)
+    mycursor.execute(table_5_cst)
+
+    table_6 = """CREATE TABLE rest (
+        rest_id int auto_increment primary key,
+        person_id int,
+        date date,
+        night_sleep_hours int default null,
+        relax_hours int default null,
+        foreign key (person_id) references personal_information(person_id)
+    )"""
+    table_6_cst = "ALTER TABLE rest MODIFY person_id int NOT NULL"
+    mycursor.execute(table_6)
+    mycursor.execute(table_6_cst)
+
+
+#### references
+# https://dev.mysql.com/doc/
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #### importing libraries
-from __future__ import print_function
-import mysql.connector
-from mysql.connector import errorcode
-import getpass
+import database_operations
 
 
 #### get root password from file
@@ -11,139 +8,19 @@ passwordFile = open(
 )
 rootPassword = passwordFile.readline()[:-1]
 
-#### get password from terminal prompt
-def getPassword():
-    try:
-        rootPassword = getpass.getpass()
-    except Exception as error:
-        print("ERROR", error)
-    return rootPassword
-
-
-#### open connection with given credentials and return the connection object
-def openConnectionToDB(host, user, password, database):
-    try:
-        connection = mysql.connector.connect(
-            host="localhost", user="root", password=rootPassword
-        )
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-    return connection
-
-
-#### close given connection
-def closeConnectionToDB(connection):
-    try:
-        connection.close()
-    except:
-        print("Encountered error while closing the connection")
-
 
 def main():
-    ####
-    connection = openConnectionToDB(
+    #### create database
+    # database_operations.createDatabase(host="localhost", user="root", password=rootPassword)
+
+    #### populate database with appropriate tables
+    database_operations.createDatabaseTables(
         host="localhost",
         user="root",
         password=rootPassword,
         database="WorkoutTrackerDB",
     )
-    #### create cursor for further usage
-    mycursor = connection.cursor()
-
-    mycursor.execute("use WorkoutTrackerDB")
-
-    #### create database
-    # mycursor.execute("CREATE DATABASE WorkoutTrackerDB")
-    
-    #### create tables
-    # table_1 = '''CREATE TABLE personal_information (
-    #     person_id int auto_increment primary key,
-    #     first_name varchar(90),
-    #     last_name varchar(90),
-    #     birth_date date,
-    #     phone_number int default null,
-    #     nationality varchar(90),
-    #     registration_date date
-    # )'''
-    # mycursor.execute(table_1)
-
-    # table_2 = '''CREATE TABLE diet (
-    #     diet_id int auto_increment primary key,
-    #     person_id int,
-    #     name varchar(90),
-    #     start_date date,
-    #     stop_date date,
-    #     calories int default null,
-    #     foreign key (person_id) references personal_information(person_id)
-    # )'''
-    # table_2_cst = "ALTER TABLE diet MODIFY person_id int NOT NULL"
-    # mycursor.execute(table_2)
-    # mycursor.execute(table_2_cst)
-
-    # table_3 = '''CREATE TABLE running (
-    #     running_id int auto_increment primary key,
-    #     person_id int,
-    #     date date,
-    #     type varchar(90),
-    #     total_time int default null,
-    #     total_distance int default null,
-    #     terrain varchar(90),
-    #     foreign key (person_id) references personal_information(person_id)
-    # )'''
-    # table_3_cst = "ALTER TABLE running MODIFY person_id int NOT NULL"
-    # mycursor.execute(table_3)
-    # mycursor.execute(table_3_cst)
-    
-    # table_4= '''CREATE TABLE swimming (
-    #     swimming_id int auto_increment primary key,
-    #     person_id int,
-    #     date date,
-    #     type varchar(90),
-    #     total_time int default null,
-    #     total_distance int default null,
-    #     water varchar(90),
-    #     foreign key (person_id) references personal_information(person_id)
-    # )'''
-    # table_4_cst = "ALTER TABLE swimming MODIFY person_id int NOT NULL"
-    # mycursor.execute(table_4)
-    # mycursor.execute(table_4_cst)
-
-    # table_5= '''CREATE TABLE weight_lifting (
-    #     weight_lifting_id int auto_increment primary key,
-    #     person_id int,
-    #     date date,
-    #     type varchar(90),
-    #     no_series int default null,
-    #     repeats_per_series int default null,
-    #     weight int default null,
-    #     foreign key (person_id) references personal_information(person_id)
-    # )'''
-    # table_5_cst = "ALTER TABLE weight_lifting MODIFY person_id int NOT NULL"
-    # mycursor.execute(table_5)
-    # mycursor.execute(table_5_cst)
-
-    # table_6= '''CREATE TABLE rest (
-    #     rest_id int auto_increment primary key,
-    #     person_id int,
-    #     date date,
-    #     night_sleep_hours int default null,
-    #     relax_hours int default null,
-    #     foreign key (person_id) references personal_information(person_id)
-    # )'''
-    # table_6_cst = "ALTER TABLE rest MODIFY person_id int NOT NULL"
-    # mycursor.execute(table_6)
-    # mycursor.execute(table_6_cst)
-
-
 
 
 if __name__ == "__main__":
     main()
-
-#### references
-# https://dev.mysql.com/doc/
