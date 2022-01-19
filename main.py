535e138212812cb1d2a064a023c0a07058d1392d
kermitsmind <kermitsmind@icloud.com>
2022-01-19 10:48:35 +0100

    created super_user

--- main.py
+++ main.py
@@ -14,13 +14,32 @@ def main():
     # database_operations.createDatabase(host="localhost", user="root", password=rootPassword)
 
     #### populate database with appropriate tables
-    database_operations.createDatabaseTables(
+    # database_operations.createDatabaseTables(
+    #     host="localhost",
+    #     user="root",
+    #     password=rootPassword,
+    #     database="WorkoutTrackerDB",
+    # )
+
+    #### set access levels in the database
+    connection = database_operations.openConnectionToDB(    
         host="localhost",
         user="root",
         password=rootPassword,
         database="WorkoutTrackerDB",
     )
 
+    mycursor = connection.cursor()
+    admin_user = '''
+    create user 'super_user'@'localhost' identified by 'super_user_password';
+    revoke all privileges, grant option from 'super_user'@'localhost';
+    grant all privileges on WorkoutTrackerDB.* to 'super_user'@'localhost'; -- remember about .* after db name
+    '''
+    # mycursor.execute(admin_user)
+
+    mycursor.execute("show grants for 'super_user'@'localhost'")
+    for x in mycursor:
+        print(x)
 
 if __name__ == "__main__":
     main()
